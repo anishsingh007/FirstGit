2e25d92889757ebce453e65e44e8420a2e655265
anishsingh007 <emailanishsingh@gmail.com>
2023-02-13 18:24:05 +0530

    asasda

---
+++ querySelector.js
@@ -0,0 +1,14 @@
+var header =document.querySelector('#main-header');
+header.getElementsByClassName.borderBottom = 'solid 4px #ccc';
+
+var input = document.querySelector('input');
+input.value = 'Helloworld';
+
+var submit = document.querySelector('input[type="submit"]');
+submit.value='send';
+var item = document.querySelector('.list-group-item');
+item.style.color = 'red'
+
+var lastItem = document.querySelector('.list-group-item:last-child');
+lastItem.style.color = 'blue';
+
