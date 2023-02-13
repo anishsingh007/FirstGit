5d0177d341662305107d2cfbeca187739e0833dc
anishsingh007 <emailanishsingh@gmail.com>
2023-02-13 20:39:44 +0530

    nibubj

---
+++ modifyingDom.js
@@ -0,0 +1,9 @@
+
+// var itemlist = document.querySelector('#items');
+// console.log(itemlist.parentNode);
+// itemlist.parentNode.style.backgroundColor = 'f4f4f4';
+
+var elem1 = document.createElement('elem');
+
+elem1.appendChild(elem);
+console.log(elem1);
